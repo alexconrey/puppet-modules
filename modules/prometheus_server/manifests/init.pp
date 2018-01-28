4dfb0561b6a3032e23f2b1373eb0a244c7ec0344
Alex Conrey <alex@conrey.us>
2018-01-28 17:07:10 -0600

    updated hiera logic for prometheus again

--- modules/prometheus_server/manifests/init.pp
+++ modules/prometheus_server/manifests/init.pp
@@ -1,12 +1,7 @@
 class prometheus_server {
   
-  $scrape_configs = hiera_hash('prometheus::scrape_config')
-
-  class { '::prometheus':
-    version => '1.8.2',
-    scrape_configs => [ $scrape_configs ],
-#    scrape_configs => [ {'job_name'=>'prometheus','scrape_interval'=> '30s','scrape_timeout'=>'30s','static_configs'=> [{'targets'=>['localhost:9090', 'localhost:9100'], 'labels'=> { 'alias'=>'Prometheus'}}]}],
-  }
+  $prometheus_config = hiera_hash('prometheus::config')
+  create_resources('prometheus', $prometheus_config
 
 }
 
