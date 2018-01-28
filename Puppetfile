77b29a45c127048c55ca843e658d0ae387771c0d
Alex Conrey <alex@conrey.us>
2018-01-28 17:59:16 -0600

    added vcsrepo module

--- Puppetfile
+++ Puppetfile
@@ -10,6 +10,7 @@ mod 'abaranov-libreswan', '0.2.2'
 mod 'puppetlabs/concat'
 mod 'puppetlabs/firewall', '1.12.0'
 mod 'pcfens-yas3fs', '0.3.3'
+mod 'puppetlabs/vcsrepo'
 mod 'dovecot',
   :git => 'https://github.com/mxhero/puppet-dovecot'
 mod 'postfix',
---
+++ modules/protectmymail_mailserver/manifests/postfix_exporter.pp
@@ -0,0 +1,13 @@
+class protectmymail_mailserver::postfix_exporter {
+  vcsrepo { '/opt/postfix_exporter':
+    ensure   => present,
+    provider => git,
+    source   => 'git://github.com/kumina/postfix_exporter',
+  }
+
+  exec { 'build postfix_exporter':
+    command    => 'bash build_static.sh',
+    cwd        => '/opt/postfix_exporter',
+    depends    => Vcsrepo['/opt/postfix_exporter'],
+  }
+}
