6951850ac6c7eefbe4280d45ff3fd9fad6ad9d82
Alex Conrey <alex@conrey.us>
2018-01-28 17:17:21 -0600

    more proemt

--- modules/prometheus_server/manifests/init.pp
+++ modules/prometheus_server/manifests/init.pp
@@ -1,7 +1,32 @@
 class prometheus_server {
   
-  $prometheus_config = hiera_hash('prometheus::config')
-  create_resources('prometheus', $prometheus_config
+#  $prometheus_config = hiera_hash('prometheus::config')
+
+#  create_resources('prometheus', $prometheus_config
+
+  class {'::prometheus':
+    version  => '1.8.2',
+    scrape_configs  =>  [
+      {
+        'job_name' => 'prometheus',
+        'scrape_interval' => '10s',
+        'scrape_timeout'  => '10s',
+        'static_configs'  => [{
+          'targets'  => ['localhost:9090'],
+          'labels'   => { 'alias' => 'Prometheus' },
+	}],
+      },
+      {
+        'job_name'  => 'noes',
+        'scrape_interval' => '10s',
+        'scrape_timeout'  => '10s',
+        'static_configs'  => [{
+          'targets'  => ['localhost:9100'],
+          'labels'   => { 'alias' => 'nodes' },
+	}],
+      }
+    ],
+  }
 
 }
 
