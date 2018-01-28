8b55c63c8e7619b1b6d0fbf0fded602c18fbfbcd
Alex Conrey <alex@conrey.us>
2018-01-27 22:16:08 -0600

    fixed syntax issues

--- modules/router/manifests/vpn_tunnel.pp
+++ modules/router/manifests/vpn_tunnel.pp
@@ -1,10 +1,10 @@
 class router::vpn_tunnel {
   
-  conns = hiera('router::tunnels')
-  secrets = hiera('router::secrets')
+  $conns = hiera('router::tunnels')
+  $secrets = hiera('router::secrets')
 
   create_resources('libreswan::conn', $conns)
-  create_resources('libreswan::secrets', $secrets)
+  create_resources('libreswan::secret', $secrets)
     
 } 
   
