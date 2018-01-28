9435004b1ae7b014c8dc65492e8a945dba0113fe
Alex Conrey <alex@conrey.us>
2018-01-28 16:56:07 -0600

    because im a dumbass

--- modules/prometheus_server/manifests/init.pp
+++ modules/prometheus_server/manifests/init.pp
@@ -6,7 +6,8 @@ class prometheus_server {
 
   class { '::prometheus':
     version => '1.8.2',
-    scrape_configs => [ {'job_name'=>'prometheus','scrape_interval'=> '30s','scrape_timeout'=>'30s','static_configs'=> [{'targets'=>['localhost:9090', 'localhost:9100'], 'labels'=> { 'alias'=>'Prometheus'}}]}],
+    scrape_configs => [ $scrape_configs ],
+#    scrape_configs => [ {'job_name'=>'prometheus','scrape_interval'=> '30s','scrape_timeout'=>'30s','static_configs'=> [{'targets'=>['localhost:9090', 'localhost:9100'], 'labels'=> { 'alias'=>'Prometheus'}}]}],
   }
 
 }
