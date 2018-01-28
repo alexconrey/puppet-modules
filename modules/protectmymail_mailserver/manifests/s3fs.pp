a33b41781f79da3ea805eaedf1866e82e39fdaa1
Alex Conrey <alex@conrey.us>
2018-01-28 00:22:16 -0600

    change init_system for s3fs

--- modules/protectmymail_mailserver/manifests/s3fs.pp
+++ modules/protectmymail_mailserver/manifests/s3fs.pp
@@ -1,6 +1,10 @@
 class protectmymail_mailserver::s3fs {
   
   $s3fs = hiera_hash('protectmymail_mailserver::s3fs', undef) 
+
+  class {'yas3fs':
+    init_system  => 'systemd',
+  }
   
   create_resources('yas3fs::mount',  $s3fs)
 
