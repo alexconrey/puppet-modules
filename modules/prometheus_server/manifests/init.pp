9b286ab4df1d075efdb1bba4759bae83bd456e40
Alex Conrey <alex@conrey.us>
2018-01-28 16:49:33 -0600

    explict version

--- modules/prometheus_server/manifests/init.pp
+++ modules/prometheus_server/manifests/init.pp
@@ -5,7 +5,8 @@ class prometheus_server {
   $scrape_configs = hiera_hash('prometheus::scrape_config')
 
   class { '::prometheus':
-    scrape_configs  => [hiera_hash('prometheus::scrape_config')],
+    version => '1.8.2',
+    scrape_configs => [ {'job_name'=>'prometheus','scrape_interval'=> '30s','scrape_timeout'=>'30s','static_configs'=> [{'targets'=>['localhost:9090', 'localhost:9100'], 'labels'=> { 'alias'=>'Prometheus'}}]}],
   }
 
 }
