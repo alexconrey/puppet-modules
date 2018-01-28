ff244e07b095cff2ccac5d06e45d70413c81c748
Alex Conrey <alex@conrey.us>
2018-01-27 23:49:16 -0600

    more hiera bullshit

--- modules/protectmymail_mailserver/manifests/s3fs.pp
+++ modules/protectmymail_mailserver/manifests/s3fs.pp
@@ -1,5 +1,7 @@
 class protectmymail_mailserver::s3fs {
- 
-  create_resources('yas3fs::mount',  $::protectmymail_mailserver::s3fs)
+  
+  $s3fs = hiera_hash('protectmymail_mailserver::s3fs', undef) 
+  
+  create_resources('yas3fs::mount',  $s3fs)
 
 }
