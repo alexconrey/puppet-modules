f72df2ccc984b3fc6036f02a919aff43d1f7a060
Alex Conrey <alex@conrey.us>
2018-01-28 21:07:52 -0600

    updated prometheus layout again

--- modules/prometheus_server/manifests/init.pp
+++ modules/prometheus_server/manifests/init.pp
@@ -1,32 +1,33 @@
 class prometheus_server {
   
 #  $prometheus_config = hiera_hash('prometheus::config')
-  $prometheus_nodes = hiera('prometheus::nodes', [])
+#  $prometheus_nodes = hiera('prometheus_server::nodes', [])
+  $prometheus_jobs = hiera_hash('prometheus_server::jobs', {})
 
 #  create_resources('prometheus', $prometheus_config
 
   class {'::prometheus':
     version  => '1.8.2',
-    scrape_configs  =>  [
-      {
-        'job_name' => 'prometheus',
-        'scrape_interval' => '10s',
-        'scrape_timeout'  => '10s',
-        'static_configs'  => [{
-          'targets'  => ['localhost:9090'],
-          'labels'   => { 'alias' => 'Prometheus' },
-	}],
-      },
-      {
-        'job_name'  => 'noes',
-        'scrape_interval' => '10s',
-        'scrape_timeout'  => '10s',
-        'static_configs'  => [{
-#          'targets'  => ['localhost:9100', '10.10.2.35:9100'],
-          'targets'  => $prometheus_nodes,
-          'labels'   => { 'alias' => 'nodes' },
-	}],
-      }
+    scrape_configs  =>  [$prometheus_jobs],
+#    scrape_configs  =>  [
+#      {
+#        'job_name' => 'prometheus',
+#        'scrape_interval' => '10s',
+#        'scrape_timeout'  => '10s',
+#        'static_configs'  => [{
+#          'targets'  => ['localhost:9090'],
+#          'labels'   => { 'alias' => 'Prometheus' },
+#	}],
+#      },
+#      {
+#        'job_name'  => 'noes',
+#        'scrape_interval' => '10s',
+#        'scrape_timeout'  => '10s',
+#        'static_configs'  => [{
+#          'targets'  => $prometheus_nodes,
+#          'labels'   => { 'alias' => 'nodes' },
+#	}],
+#      }
     ],
   }
 
