def3d60ed319486edf40b7ff1154010fb0a88ca9
Alex Conrey <alex@conrey.us>
2018-01-28 22:47:31 -0600

    fixed blackbox_exporter

--- modules/prometheus_exporters/manifests/blackbox.pp
+++ modules/prometheus_exporters/manifests/blackbox.pp
@@ -1,7 +1,10 @@
 class prometheus_exporters::blackbox(
   $modules = hiera_hash('prometheus_exporters::blackbox::modules', {})
 ){
-  $resource_hash = { 'modules' => $modules } 
-  create_resources('prometheus::blackbox_exporter', $resource_hash) 
+  
+  class {'prometheus::blackbox_exporter':
+    modules => $modules,
+  }
+
 
 }
