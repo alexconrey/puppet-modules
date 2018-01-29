20badadd37db087f66d857241a3c54f9b2c3c9ca
Alex Conrey <alex@conrey.us>
2018-01-28 21:47:48 -0600

    updated final bit for hiera based jobs in prometheus

--- modules/prometheus_server/manifests/init.pp
+++ modules/prometheus_server/manifests/init.pp
@@ -2,33 +2,13 @@ class prometheus_server {
   
 #  $prometheus_config = hiera_hash('prometheus::config')
 #  $prometheus_nodes = hiera('prometheus_server::nodes', [])
-  $prometheus_jobs = hiera_hash('prometheus_server::jobs')
+  $prometheus_jobs = hiera('prometheus_server::jobs')
 
 #  create_resources('prometheus', $prometheus_config
 
   class {'::prometheus':
     version  => '1.8.2',
-    scrape_configs  =>  [$prometheus_jobs],
-    scrape_configs  =>  [
-      {
-        'job_name' => 'prometheus',
-        'scrape_interval' => '10s',
-        'scrape_timeout'  => '10s',
-        'static_configs'  => [{
-          'targets'  => ['localhost:9090'],
-          'labels'   => { 'alias' => 'Prometheus' },
-	}],
-      }, $prometheus_jobs ],
-#      {
-#        'job_name'  => 'noes',
-#        'scrape_interval' => '10s',
-#        'scrape_timeout'  => '10s',
-#        'static_configs'  => [{
-#          'targets'  => $prometheus_nodes,
-#          'labels'   => { 'alias' => 'nodes' },
-#	}],
-#      }
-#    ],
+    scrape_configs  =>  $prometheus_jobs,
   }
 
 }
